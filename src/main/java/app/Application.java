9453f2771d88b811a65b5a218512147953402903
kelvinikome <kvnikome@gmail.com>
2018-01-10 11:12:39 +0100

    LTE-App

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,16 @@
 package app;
 
+import java.io.IOException;
 import java.util.Arrays;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -24,7 +33,20 @@ import app.service.ProfileService;
 
 @SpringBootApplication
 @EnableWebSecurity
-public class Application extends WebSecurityConfigurerAdapter {
+public class Application extends WebSecurityConfigurerAdapter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
  
     private static String REALM="MY_TEST_REALM";
     
